b89f45966533616d27a03131385e60eef27d4655
sreenivasnaresh <96224983+sreenivasnaresh@users.noreply.github.com>
2023-09-28 20:23:12 +0530

    Update nexus_download_artifacts.groovy

--- vars/nexus_download_artifacts.groovy
+++ vars/nexus_download_artifacts.groovy
@@ -1,8 +1,8 @@
-def call(fileName,groupID,artiID){
+def call(fileName,groupID,artiID,nexusRepo){
   def pom = readMavenPom file: 'pom.xml'
   def ver = pom.version 
   def grpID =groupID.replace(".", "/")
   sh """
-     curl -o ${fileName} -u $USERPASS -X GET "${env.nexus_url}/repository/doctor-online-release/${grpID}/${artiID}/${ver}/doctor-online-${ver}.war"
+     curl -o ${fileName} -u $USERPASS -X GET "${env.nexus_url}/repository/${nexusRepo}/${grpID}/${artiID}/${ver}/doctor-online-${ver}.war"
   """
 }
